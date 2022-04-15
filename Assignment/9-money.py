95016dfc13f09a60fa52540e939aaf73ee2db73c
이제호 <78259314+jhl8109@users.noreply.github.com>
2022-04-15 20:06:32 +0900

    prof.zoh assignment-9 money.py

--- Assignment/9-money.py
+++ Assignment/9-money.py
@@ -1,12 +1,11 @@
 import sys
 
-
 inp = input();
 tokens = [];
-
 tokens= inp.split(' ');
 tokens.insert(0,'0');
 N = len(tokens);
+
 dpMax = [[1 for j in range(int(N/2) +1)] for i in range(int(N/2) +1)];
 dpMin = [[1 for j in range(int(N/2) +1)] for i in range(int(N/2) +1)];
 visited = [[False]*(int(N/2)+1) for i in range(int(N/2)+1)];
@@ -28,8 +27,8 @@ def makeTable(index1, index2):
         dpMin[index1][index2] = calculator(int(tokens[index1 * 2 - 1]), int(tokens[index2 * 2 - 1]), tokens[index1 * 2]);
     else :
         gap = index2 - index1;
-        big = -100000000;
-        absBig = 10000000;
+        big = sys.maxsize * -1;
+        absBig = sys.maxsize;
         for i in range(0,gap) :
             if (visited[index1][index1 + i] == False) :
                 makeTable(index1, index1 + i);
@@ -39,21 +38,13 @@ def makeTable(index1, index2):
             big = max(big, calculator(dpMin[index1][index1 + i], dpMin[index1 + i + 1][index2], tokens[(index1 + i) * 2]));
             big = max(big, calculator(dpMax[index1][index1 + i], dpMin[index1 + i + 1][index2], tokens[(index1 + i) * 2]));
             big = max(big, calculator(dpMin[index1][index1 + i], dpMax[index1 + i + 1][index2], tokens[(index1 + i) * 2]));
-            absBig = min(absBig, calculator(dpMax[index1][index1 + i], dpMax[index1 + i + 1][index2], tokens[(index1 + i) * 2]));                absBig = min(absBig, calculator(dpMin[index1][index1 + i], dpMin[index1 + i + 1][index2], tokens[(index1 + i) * 2]));
+            absBig = min(absBig, calculator(dpMax[index1][index1 + i], dpMax[index1 + i + 1][index2], tokens[(index1 + i) * 2]));   
+            absBig = min(absBig, calculator(dpMin[index1][index1 + i], dpMin[index1 + i + 1][index2], tokens[(index1 + i) * 2]));                absBig = min(absBig, calculator(dpMin[index1][index1 + i], dpMin[index1 + i + 1][index2], tokens[(index1 + i) * 2]));
             absBig = min(absBig, calculator(dpMax[index1][index1 + i], dpMin[index1 + i + 1][index2], tokens[(index1 + i) * 2]));
             absBig = min(absBig, calculator(dpMin[index1][index1 + i], dpMax[index1 + i + 1][index2], tokens[(index1 + i) * 2]));   
-            dpMax[index1][index2] = big;
-            dpMin[index1][index2] = absBig;
+        dpMax[index1][index2] = big;
+        dpMin[index1][index2] = absBig;
     visited[index1][index2] = True;
 
-makeTable(1,int(N/2));
-#print(tokens);
-#for i in range(0,len(dpMax)) :
-#    print(dpMax[i]);
-#print("\n");  
-#for i in range(0,len(dpMin)) :
-#    print(dpMin[i]);
-#print("\n");      
+makeTable(1,int(N/2));   
 print(dpMax[1][int(N/2)]);
-
-
