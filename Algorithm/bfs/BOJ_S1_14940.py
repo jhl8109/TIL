03becce6d233fd2e4f43b18c904caec68f2ef918
jhl8109 <jhl81094@gmail.com>
2023-04-09 23:34:52 +0900

    BOJ_S1_14940 bfs

---
+++ Algorithm/bfs/BOJ_S1_14940.py
@@ -0,0 +1,45 @@
+from collections import deque
+
+n,m = map(int, input().split())
+board =[]
+answer = [[-1] * m for _ in range(n)]
+visited =[[False] * m for _ in range(n)]
+dx = [0,0,-1,1]
+dy = [-1,1,0,0]
+
+queue = deque()
+
+start = (10000,10000)
+for i in range(n):
+    inp = list(map(int,input().split()))
+
+    if start == (10000,10000) and 2 in inp:
+        for j in range(m):
+            if inp[j] == 2:
+                start = (i,j)
+
+    board.append(inp)
+
+def bfs(y,x):
+    board[y][x] = 0
+    visited[y][x] = True
+    queue.append((y,x,0))
+    while queue:
+        (cur_y,cur_x,cur_dist) = queue.popleft()
+        for i in range(4):
+            nx = cur_x + dx[i]
+            ny = cur_y + dy[i]
+            if nx < 0 or nx >= m or ny < 0 or ny >= n:
+                continue
+            if visited[ny][nx] or board[ny][nx] == 0:
+                continue
+            queue.append((ny,nx,cur_dist+1))
+            board[ny][nx] = cur_dist+1
+            visited[ny][nx] = True
+
+bfs(start[0],start[1])
+
+for i in range(n):
+    for j in range(m):
+        print(board[i][j], end=' ')
+    print()
