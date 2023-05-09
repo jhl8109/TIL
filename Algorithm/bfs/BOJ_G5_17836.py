30a304b0cdcbfa2361a41037f0b34275386b4b55
jhl8109 <jhl81094@gmail.com>
2023-05-10 00:22:07 +0900

    BOJ_G5_17836 bfs

--- Algorithm/bfs/BOJ_G5_17836.py
+++ Algorithm/bfs/BOJ_G5_17836.py
@@ -11,26 +11,30 @@ for _ in range(N):
     board.append(tmp)
 
 def solve(start,end,visited):
+    gram_dist = 0
+    ans = 10001
     queue = deque([start])
     visited[start[1]][start[2]] = 1
-    chance = 0
     while len(queue) != 0:
         cur = queue.popleft()
         if cur[1] == end[1] and cur[2] == end[2]:
-            return cur[0]
-        if board[cur[1]][cur[2]] == 2:
-            chance = 1
+            ans = min(ans,cur[0])
         for i in range(4):
             ny = dy[i] + cur[1]
             nx = dx[i] + cur[2]
             if ny < N and ny >= 0 and nx < M and nx >= 0 and visited[ny][nx] == 0:
-                if chance == 1 or board[ny][nx] !=1:
+                if board[ny][nx] == 0:
                     queue.append((cur[0] + 1, ny,nx))
                     visited[ny][nx] = 1
-    return end[0] + 1
+                if board[ny][nx] == 2:
+                    queue.append((cur[0] + 1, ny,nx))
+                    gram_dist = cur[0] + 1 + abs(ny-end[1]) + abs(nx-end[2])
+                    ans = min(gram_dist,ans)
+                    visited[ny][nx] = 1
+    return ans
 
-ans = solve((0,0,0),(T,N-1,M-1),visited)
-if ans < T:
-    print(ans)
+ret = solve((0,0,0),(T,N-1,M-1),visited)
+if ret <= T:
+    print(ret)
 else:
     print("Fail")
