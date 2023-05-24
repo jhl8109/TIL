ef2d3e1120aeb1b554866018de90a21b529f0875
jhl8109 <jhl81094@gmail.com>
2023-05-25 00:21:25 +0900

    BOJ_G5_2493 stack

--- Algorithm/dataStructure/BOJ_G5_2493.py
+++ Algorithm/dataStructure/BOJ_G5_2493.py
@@ -1,4 +1,15 @@
 N = int(input())
 arr = list(map(int, input().split()))
-
-print(N,arr)
+stack = []
+answer = [0] * N
+for i in range(N):
+    ans = 0
+    while stack:
+        if stack[-1][1] > arr[i]:
+            ans = stack[-1][0] + 1
+            break
+        else:
+            stack.pop()
+    stack.append((i,arr[i]))
+    answer[i] = ans
+print(" ".join(map(str,answer)))
