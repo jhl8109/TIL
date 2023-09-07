a6269f3e0a8d1397df2a74ba1dedd26d2ba78fa6
jhl8109 <jhl81094@gmail.com>
2023-09-07 17:16:31 +0900

    BOJ_S2_2512 구현

---
+++ Algorithm/implementation/BOJ_S2_2512.java
@@ -0,0 +1,47 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+    public static int N, max;
+    public static Integer[] arr;
+
+    public static int solution(int sum) {
+        int substract = sum - max;
+        for(int i=0; i<N; i++) {
+            int partialSum = 0, limited = 0;
+            for(int j=0; j<=i; j++){
+                partialSum += arr[j];
+            }
+            limited = (partialSum-substract)/(i+1); // 그 partialSum - 부분 합이 = substract
+            if (i+1 == N || limited >= arr[i+1]) return limited;
+            else continue;
+        }
+        return 0;
+    }
+    public static void main(String[] args) throws IOException{
+        Main main = new Main();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        arr = new Integer[N];
+
+        st = new StringTokenizer(br.readLine());
+        
+        int sum = 0;
+        // Integer[] arr2 = Arrays.stream(arr).boxed().toArray(Integer[]::new);
+        // arr = Arrays.sort(arr2,Collections.reverseOrder());
+       
+        for(int i=0; i<N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+            sum += arr[i];
+        }
+        Arrays.sort(arr,Collections.reverseOrder());
+        st = new StringTokenizer(br.readLine());
+        max = Integer.parseInt(st.nextToken());
+
+        if(sum <= max) System.out.println(arr[0]);
+        else System.out.println(solution(sum));
+
+    }
+}
