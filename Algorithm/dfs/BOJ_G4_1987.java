7ad59fab17ba916fec2fd26c31080d0df86c6381
jhl8109 <jhl81094@gmail.com>
2023-10-03 18:07:09 +0900

    BOJ_G4_1987 DFS

---
+++ Algorithm/dfs/BOJ_G4_1987.java
@@ -0,0 +1,44 @@
+import java.util.*;
+import java.io.*;
+
+class Main{
+	static int R,C;
+	static int[] ny = {1,-1,0,0};
+	static int[] nx = {0,0,-1,1};
+	static int max = 1;
+
+	public static boolean checkBoundary(int dy,int dx) {
+		if(dy >= 0 && dy < R && dx >= 0 && dx < C) return true;
+		else return false;
+	}
+
+	public static void DFS(char[][] board, StringBuilder sb, int y, int x) {
+		// System.out.println(sb.toString());
+		for(int i=0; i<4; i++) {
+			int dy = y + ny[i];
+			int dx = x + nx[i];
+			
+			if(checkBoundary(dy,dx) && !sb.toString().contains(Character.toString(board[dy][dx]))){
+				sb.append(board[dy][dx]);
+				max = Math.max(max, sb.length());
+				DFS(board,sb,dy,dx);
+				sb.delete(sb.length()-1, sb.length());
+			}
+		}
+	}
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		R = Integer.parseInt(st.nextToken());
+		C = Integer.parseInt(st.nextToken());
+		char[][] board = new char[R][C];
+
+		for(int i=0; i<R; i++) {
+			board[i] = br.readLine().toCharArray();
+		}
+		StringBuilder sb = new StringBuilder();
+		sb.append(board[0][0]);
+		DFS(board,sb, 0,0);
+		System.out.println(max);
+	}
+}
